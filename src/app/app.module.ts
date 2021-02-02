995cd575bff1e0b15f45a4134aaecd50f1f2c244
dsteyn73 <dsteyn.career@gmail.com>
2021-02-01 20:08:22 -0700

    1 file modified delet service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ComponentService } from './component.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [ComponentService]
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
